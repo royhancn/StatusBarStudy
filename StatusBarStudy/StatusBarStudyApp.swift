6f371ff013dccda69787cd260be0bf670f03d49c
Roy HAN <royhancn@outlook.com>
2023-10-15 13:22:53 +0800

    Initial Commit

---
+++ StatusBarStudy/StatusBarStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusBarStudyApp.swift
+//  StatusBarStudy
+//
+//  Created by Roy on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct StatusBarStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
